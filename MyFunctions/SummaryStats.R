376e92162946c7a0b931ade02e3f9d016c1687d2
John Hamm <hammjm125@yahoo.com>
2017-08-08 14:42:30 -0400

    Update and rename MeanMedianModeRange.R to SummaryStats.R

---
+++ MyFunctions/SummaryStats.R
@@ -0,0 +1,60 @@
+# Summary Statistics Function
+
+#From sum.R, as I wanted to use my own function
+my_sum <- function(x) {
+  mysum <- 0
+  for (i in x) {
+    mysum <- mysum + i
+  }
+  print(mysum)
+}
+
+# Custom maximum function
+my_max <- function(x) {
+  maxvalue <- -1/0.00000000000000000000000000000001 #approaching negative infinity
+  for (i in x) {
+    if (i <= maxvalue)
+      next
+    else (maxvalue <- i)
+      
+  }
+  print(maxvalue)
+}
+
+# Custom min function
+my_min <- function(x) {
+  minvalue <- 10000000000000000000000
+  for (i in x) {
+    if (i >= minvalue)
+      next
+    else (minvalue <- i)
+    
+  }
+  print(minvalue)
+}
+
+
+# Mean
+my_mean <- function(x) {
+  
+  print('Calculating Sum...')
+  numer <- my_sum(x)
+  denom <- length(x)
+  
+  print('Calculating Mean...')
+  mymean <- numer/denom
+  print(mymean)
+  
+}
+
+# Median
+my_median <- function(x){
+  }
+# Mode
+my_mode <- function(x){
+  }
+
+# Range
+my_range <- function(x){
+  myrange <- my_max(x) - my_min(x)
+}
